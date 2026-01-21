a1b6bfba52073a7c35e5e82c0c6001739636f4e2
XianSarmiento <jcmsarmiento07@gmail.com>
2026-01-21 16:17:06 +0800

    Implement validation for required fields

--- MemberManagement.Domain/Member.cs
+++ MemberManagement.Domain/Member.cs
@@ -9,10 +9,13 @@ namespace MemberManagement.Domain
         public int MemberID { get; set; }
 
         // Core Member Data
+        [Required(ErrorMessage = "First Name is required.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; }
 
-        // public DateOnly BirthDate { get; set; }
+        // FOR INSTANCES LANG: [Required(ErrorMessage = "BirthDate is required.")]
+        [BirthDateValidation(ErrorMessage = "BirthDate cannot be in the future.")]
         public DateTime BirthDate { get; set; }
 
         public string Address { get; set; }
@@ -23,5 +26,22 @@ namespace MemberManagement.Domain
         public string EmailAddress { get; set; }
         public bool IsActive { get; set; } 
         public DateTime DateCreated { get; set; }
+
+
+        // Custom Validation Attribute for BirthDate
+        private class BirthDateValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value is DateTime birthDate)
+                {
+                    if (birthDate > DateTime.Now)
+                    {
+                        return new ValidationResult(ErrorMessage ?? "Birthdate cannot be in the future.");
+                    }
+                }
+                return ValidationResult.Success;
+            }
+        }
     }
 }
