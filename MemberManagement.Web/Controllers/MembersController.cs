95afa228bc7d05fce4fad9bccfb262d4237ce71d
XianSarmiento <jcmsarmiento07@gmail.com>
2026-01-22 15:18:18 +0800

    Setup Member Controller and ViewModel

--- MemberManagement.Web/Controllers/MembersController.cs
+++ MemberManagement.Web/Controllers/MembersController.cs
@@ -1,4 +1,6 @@
 ﻿using MemberManagement.Application.Services;
+using MemberManagement.Domain.Entities;
+using MemberManagement.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemberManagement.Web.Controllers
@@ -15,7 +17,50 @@ namespace MemberManagement.Web.Controllers
         public async Task<IActionResult> Index()
         {
             var members = await memberService.GetActiveMembersAsync();
+
+            // add this, right after creating viewmodel
+            var vm = members.Select(m => new MemberVM
+            {
+                MemberID = m.MemberID,
+                FirstName = m.FirstName,
+                LastName = m.LastName,
+                BirthDate = m.BirthDate,
+                Address = m.Address,
+                Branch = m.Branch,
+                ContactNo = m.ContactNo,
+                EmailAddress = m.EmailAddress
+            });
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MemberVM vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var member = new Member
+            {
+                FirstName = vm.FirstName,
+                LastName = vm.LastName,
+                BirthDate = vm.BirthDate,
+                Address = vm.Address,
+                Branch = vm.Branch,
+                ContactNo = vm.ContactNo,
+                EmailAddress = vm.EmailAddress
+            };
+
+            await this.memberService.CreateAsync(member);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ MemberManagement.Web/ViewModels/MemberVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemberManagement.Web.ViewModels
+{
+    public class MemberVM
+    {
+        public int MemberID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateOnly BirthDate { get; set; }
+        public string Address { get; set; }
+        public string Branch { get; set; }
+
+        public string ContactNo { get; set; }
+        public string EmailAddress { get; set; }
+
+
+        // Include these only if displaying them
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
