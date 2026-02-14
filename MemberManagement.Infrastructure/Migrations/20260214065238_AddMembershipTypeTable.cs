6510e334b4328a141ce96a75567585e237f08164
XianSarmiento <jcmsarmiento07@gmail.com>
2026-02-14 14:57:37 +0800

    chore(migration): add membership table migration

---
+++ MemberManagement.Infrastructure/Migrations/20260214065238_AddMembershipTypeTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMembershipTypeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MembershipTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    MembershipFee = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MembershipTypes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MembershipTypes");
+        }
+    }
+}
